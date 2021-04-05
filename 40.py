8d84d5bb63ca4a92c88b384d08b28eecf14197ac
Mogutov I <maison1@mail.ru>
2021-04-05 16:00:38 +0600

    40

---
+++ 40.py
@@ -0,0 +1,53 @@
+from xml.etree import ElementTree as El
+
+ex = input()
+ans = {"red": 0, "green": 0, "blue": 0}
+xml = El.fromstring(ex)
+
+
+def count_cost(root, cost):
+    ans[root.get('color')] += cost
+    for _ in root:
+        count_cost(_, cost + 1)
+
+
+count_cost(xml, 1)
+for _ in ans.values():
+    print(_, end=' ')
+
+'''
+
+Вам дано описание пирамиды из кубиков в формате XML.
+Кубики могут быть трех цветов: красный (red), зеленый (green) и синий (blue﻿).
+Для каждого кубика известны его цвет, и известны кубики, расположенные прямо под ним.
+
+Пример:
+
+<cube color="blue">
+  <cube color="red">
+    <cube color="green">
+    </cube>
+  </cube>
+  <cube color="red">
+  </cube>
+</cube>
+
+ 
+
+Введем понятие ценности для кубиков. Самый верхний кубик, соответствующий корню XML документа имеет ценность 1. 
+Кубики, расположенные прямо под ним, имеют ценность 2. Кубики, расположенные прямо под нижележащими кубиками, 
+имеют ценность 3. И т. д.
+
+Ценность цвета равна сумме ценностей всех кубиков этого цвета.
+
+Выведите через пробел три числа: ценности красного, зеленого и синего цветов.
+
+Sample Input:
+
+<cube color="blue"><cube color="red"><cube color="green"></cube></cube><cube color="red"></cube></cube>
+
+Sample Output:
+
+4 3 1
+
+'''
